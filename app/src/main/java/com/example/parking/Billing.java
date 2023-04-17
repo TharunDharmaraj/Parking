367cef3130ef4865931d8de6a5507a4a8277768b
Tharun <83175935+TharunDharmaraj@users.noreply.github.com>
2023-04-17 18:09:25 +0530

    Bug Fixed

--- app/src/main/java/com/example/parking/Billing.java
+++ app/src/main/java/com/example/parking/Billing.java
@@ -22,15 +22,13 @@ import com.google.firebase.firestore.FirebaseFirestore;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.Duration;
-import java.time.LocalDateTime;
-import java.time.format.DateTimeFormatter;
 import java.util.Date;
 
 public class Billing extends AppCompatActivity {
 
     Button btn;
     String inTime, outTime;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,64 +43,82 @@ public class Billing extends AppCompatActivity {
         TextView innTime = findViewById(R.id.inTime);
         TextView outtTime = findViewById(R.id.outTime);
         TextView hoursSpent = findViewById(R.id.hoursSpent);
-        CollectionReference usersRef = firestore.collection("RFIDS");
+        if (rfid.equals("RFID1")) {
+            inTime = bundle.getString("InTime");
+            outTime = bundle.getString("OutTime");
+            innTime.setText(inTime);
+            outtTime.setText(outTime);
+            // Parse the inTime and outTime strings into Date objects
+            DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd MM yyyy");
 
-        DocumentReference rfidDocRef = usersRef.document(rfid);
+            Date inTimeDate = null;
+            try {
+                inTimeDate = dateFormat.parse(inTime);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            Date outTimeDate = null;
+            try {
+                outTimeDate = dateFormat.parse(outTime);
+            } catch (ParseException e) {
+                throw new RuntimeException(e);
+            }
+            long timeInside = outTimeDate.getTime() - inTimeDate.getTime();
+            double billPerHour = 05.0; // change this to the rate you want to charge per minute
+            double totalBill = (timeInside / 60000.0) * billPerHour; // convert milliseconds to minutes
+            TextView Bill = findViewById(R.id.yourBill);
+            long hours = (long) timeInside / 3600000;
+            hoursSpent.setText(Long.toString(hours) + " Hours spent");
+            Bill.setText(String.valueOf(totalBill));
+        } else {
+            CollectionReference usersRef = firestore.collection("RFIDS");
 
-        rfidDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
-            @Override
-            public void onSuccess(DocumentSnapshot documentSnapshot) {
-                if (documentSnapshot.exists()) {
-                    if(rfid.equals("RFID1")){
-                        inTime = bundle.getString("InTime");
-                        outTime = bundle.getString("OutTime");
-                        innTime.setText(inTime);
-                        outtTime.setText(outTime);
-                    }
-                    else{
+            DocumentReference rfidDocRef = usersRef.document(rfid);
+
+            rfidDocRef.get().addOnSuccessListener(new OnSuccessListener<DocumentSnapshot>() {
+                @Override
+                public void onSuccess(DocumentSnapshot documentSnapshot) {
+                    if (documentSnapshot.exists()) {
+
                         String inTime = documentSnapshot.getString("InTime");
                         String outTime = documentSnapshot.getString("OutTime");
                         innTime.setText(inTime);
                         outtTime.setText(outTime);
-                    }
 
-                    // Parse the inTime and outTime strings into Date objects
-                    DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd MM yyyy");
 
-                    Date inTimeDate = null;
-                    try {
-                        inTimeDate = dateFormat.parse(inTime);
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
+                        // Parse the inTime and outTime strings into Date objects
+                        DateFormat dateFormat = new SimpleDateFormat("HH:mm:ss dd MM yyyy");
+
+                        Date inTimeDate = null;
+                        try {
+                            inTimeDate = dateFormat.parse(inTime);
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+                        Date outTimeDate = null;
+                        try {
+                            outTimeDate = dateFormat.parse(outTime);
+                        } catch (ParseException e) {
+                            throw new RuntimeException(e);
+                        }
+                        long timeInside = outTimeDate.getTime() - inTimeDate.getTime();
+                        double billPerHour = 05.0; // change this to the rate you want to charge per minute
+                        double totalBill = (timeInside / 60000.0) * billPerHour; // convert milliseconds to minutes
+                        TextView Bill = findViewById(R.id.yourBill);
+                        long hours = (long) timeInside / 3600000;
+                        hoursSpent.setText(Long.toString(hours) + " Hours spent");
+                        Bill.setText(String.valueOf(totalBill));
+                    } else {
+                        Log.d("TimeTAG", "No such document");
                     }
-                    Date outTimeDate = null;
-                    try {
-                        outTimeDate = dateFormat.parse(outTime);
-                    } catch (ParseException e) {
-                        throw new RuntimeException(e);
-                    }
-                    long timeInside = outTimeDate.getTime() - inTimeDate.getTime();
-                    double billPerHour = 05.0; // change this to the rate you want to charge per minute
-                    double totalBill = (timeInside / 60000.0) * billPerHour; // convert milliseconds to minutes
-                    TextView Bill = findViewById(R.id.yourBill);
-                    long hours = (long) timeInside / 3600000;
-                    hoursSpent.setText(Long.toString(hours)+" Hours spent");
-                    Bill.setText(String.valueOf(totalBill));
-                } else {
-                    Log.d("TimeTAG", "No such document");
                 }
-            }
-        }).addOnFailureListener(new OnFailureListener() {
-            @Override
-            public void onFailure(@NonNull Exception e) {
-                Log.d("TimeTAG", "Error getting document: " + e);
-            }
-        });
-        Log.d("TimeTAG", "Document reference: " + rfidDocRef.getPath());
-        Log.d("TimeTAG", "InTime TextView found: " + (innTime != null));
-        Log.d("TimeTAG", "OutTime TextView found: " + (outtTime != null));
-
-
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    Log.d("TimeTAG", "Error getting document: " + e);
+                }
+            });
+        }
 
 
         btn.setOnClickListener(new View.OnClickListener() {
